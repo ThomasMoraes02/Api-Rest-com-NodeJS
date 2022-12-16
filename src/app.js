ab50404078cfd5ecd91547cc2006dd47e641f652
Thomas Moraes <tho.vini7@gmail.com>
2022-12-16 14:00:03 -0300

    Endpoint de atualizar livro

--- src/app.js
+++ src/app.js
@@ -19,9 +19,24 @@ app.get('/books', (req, res) => {
     res.status(200).json(books);
 });
 
+app.get('/books/:id', (req, res) => {
+    const index = findBook(req.params.id);
+    res.json(books[index]);
+});
+
 app.post('/books', (req, res) => {
     books.push(req.body);
     res.status(201).send('Livro cadastrado com sucesso');
 });
+
+app.put('/books/:id', (req, res) => {
+    const index = findBook(req.params.id);
+    books[index].title = req.body.title;
+    res.json(books);
+});
+
+function findBook(id) {
+    return books.findIndex(book => book.id == id);
+}
 
 export default app; 
