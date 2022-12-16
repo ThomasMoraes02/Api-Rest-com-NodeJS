455d846d6f6cb2cf81a4df0409362d62a2aee5fa
Thomas Moraes <tho.vini7@gmail.com>
2022-12-16 15:44:27 -0300

    Conectando ao mongodb atlas atraves do mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 // Iniciando com o express
 import express from "express";
+import db from "./config/dbConnect.js";
+
+db.on("error", console.log.bind(console, 'Erro de conexão'));
+db.once("open", () => {
+    console.log("Conexão feita com sucesso");
+})
 
 const app = express();
 
